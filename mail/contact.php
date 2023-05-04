e9fcb3b8f6e44ec08f5a3f840db11856d260fdf2
albert-yego <ayego45@gmail.com>
2023-05-04 18:10:50 +0300

    new

---
+++ mail/contact.php
@@ -0,0 +1,22 @@
+<?php
+if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+  http_response_code(500);
+  exit();
+}
+
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email = strip_tags(htmlspecialchars($_POST['email']));
+$m_subject = strip_tags(htmlspecialchars($_POST['subject']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+
+$to = "emrecanoner67@gmail.com"; // Change this email to your //
+$subject = "$m_subject:  $name";
+$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
+$headers = array("From $email", // missing colon
+    "Reply To: $email",      // missing hyphen
+    "X-Mailer: "PHP"/" . PHP_VERSION      // bad quotes
+);	
+
+if(!mail($to, $subject, $body, $headers))
+  http_response_code(500);
+?>
